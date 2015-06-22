8825cbb9575ddbad0ffb700ecc19e4f2fc5670a5
pankaj0608 <pakr0608@gmail.com>
2015-06-22 19:01:28 -0400

    Learn Core Java

--- src/basic/RotateMatrix.java
+++ src/basic/RotateMatrix.java
@@ -7,7 +7,23 @@ public class RotateMatrix {
 
     public static void main(String[] args) {
 
+        int matrix[][] = {{1,2,3},{4,5,6}, {7,8,9}};
+
+        printMatrix(matrix);
+
+        rotateMatrix(matrix);
+    }
+
+    private static void rotateMatrix(int matrix[][]) {
+
+    }
 
 
+    private static void printMatrix(int matrix[][]) {
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0 ;j<matrix[i].length; j++ )
+                System.out.print(matrix[i][j] + " ");
+            System.out.println("");
+        }
     }
 }
